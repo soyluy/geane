cbbc5ec10185dbff04b0082e43aa805a0352e080
soyluy <ysssoylu@gmail.com>
2025-06-16 02:17:08 +0300

    Refactor default create app\n Refactored the root app module created by Nx to remove unnecessary parts

--- apps/backend/users-service/src/app/app.service.ts
+++ apps/backend/users-service/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Hello API Wow this is cool' };
+    return { message: 'The server is running!' };
   }
 }
